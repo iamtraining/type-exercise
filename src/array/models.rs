564e497ce4c1c06de04440bfd5b97b36875d6ad5
Шамонин Денис <shamonin.denis@wildberries.ru>
2022-07-10 22:33:29 +0300

    заметка

--- src/array/models.rs
+++ src/array/models.rs
@@ -1,6 +1,7 @@
 use super::iterator::ArrayIterator;
 
 pub trait ArrayBuilder {
+    // тут то же самое что и в Array -> type Builder
     type Array: Array<Builder = Self>;
 
     fn with_capacity(cap: usize) -> Self;
@@ -13,6 +14,10 @@ pub trait ArrayBuilder {
 pub trait Array: Send + Sync + Sized + 'static {
     type RefItem<'a>: Copy;
 
+    // вот эту хрень я не понимаю. если убрать `<Array = Self>` то пишет ошибку
+    // expected type parameter `O`
+    // found associated type
+    // `<<O as array::models::Array>::Builder as array::models::ArrayBuilder>::Array`
     type Builder: ArrayBuilder<Array = Self>;
 
     fn get(&self, idx: usize) -> Option<Self::RefItem<'_>>;
